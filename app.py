29b5f1623ea9d2e952a6921d5646858040bd89aa
gbhasin518 <gbhasin@umd.edu>
2024-03-28 22:27:21 -0400

    Main application commit has been added for the favorite course file, contact form file and base file.

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -18,6 +18,20 @@ def about():
 @app.route('/about-css')
 def aboutcss():
     return render_template('about-css.html')
+
+@app.route('/favorite-course')
+def favoritecourse():
+    subject = request.args.get('subject')
+    course_number = request.args.get('course_number')
+    print('You entered your favorite course as: ' + request.args.get('subject') + request.args.get('course_number'))
+    return render_template('favorite-course.html', subject = subject, course_number = course_number)
+
+@app.route('/contact', methods=['GET', 'POST'])
+def contact():
+    if request.method == 'POST':
+        return render_template('contact.html', form_submitted=True)
+    else:
+        return render_template('contact.html')
 
 if __name__ == '__main__':
     app.run()
